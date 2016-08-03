2eacff0b7223a5ffa28d422a868608c4fcba2c11
Williams Medina <williams.medinaa@gmail.com>
2016-08-02 20:28:12 -0400

    debugger test

--- spec/theme-spec.js
+++ spec/theme-spec.js
@@ -1,28 +1,28 @@
 describe('One Dark UI theme', function () {
-  
+
   beforeEach(function () {
     return waitsForPromise(function () {
-      return atom.packages.activatePackage('one-dark-ui')
+      return atom.packages.activatePackage('atom-bugs-ui')
     })
   })
 
   it('allows the font size to be set via config', function () {
     expect(document.documentElement.style.fontSize).toBe('')
-    atom.config.set('one-dark-ui.fontSize', '10')
+    atom.config.set('atom-bugs-ui.fontSize', '10')
     expect(document.documentElement.style.fontSize).toBe('10px')
-    atom.config.set('one-dark-ui.fontSize', 'Auto')
+    atom.config.set('atom-bugs-ui.fontSize', 'Auto')
     return expect(document.documentElement.style.fontSize).toBe('')
   })
 
   it('allows the layout mode to be set via config', function () {
-    expect(document.documentElement.getAttribute('theme-one-dark-ui-layoutmode')).toBe('auto')
-    atom.config.set('one-dark-ui.layoutMode', 'Spacious')
-    return expect(document.documentElement.getAttribute('theme-one-dark-ui-layoutmode')).toBe('spacious')
+    expect(document.documentElement.getAttribute('theme-atom-bugs-ui-layoutmode')).toBe('auto')
+    atom.config.set('atom-bugs-ui.layoutMode', 'Spacious')
+    return expect(document.documentElement.getAttribute('theme-atom-bugs-ui-layoutmode')).toBe('spacious')
   })
 
   it('allows the tab sizing to be set via config', function () {
-    expect(document.documentElement.getAttribute('theme-one-dark-ui-tabsizing')).toBe('auto')
-    atom.config.set('one-dark-ui.tabSizing', 'Minimum')
-    return expect(document.documentElement.getAttribute('theme-one-dark-ui-tabsizing')).toBe('minimum')
+    expect(document.documentElement.getAttribute('theme-atom-bugs-ui-tabsizing')).toBe('auto')
+    atom.config.set('atom-bugs-ui.tabSizing', 'Minimum')
+    return expect(document.documentElement.getAttribute('theme-atom-bugs-ui-tabsizing')).toBe('minimum')
   })
 })
---
+++ test.js
@@ -0,0 +1,13 @@
+// Greetings
+function Greet (msg) {
+  console.log(msg)
+}
+// Bye
+function Bye (msg) {
+  console.log(msg)
+}
+
+// hello
+Greet('Hello World')
+// bye
+Bye('finish')
